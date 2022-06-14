fd83b8b59c68046ebdb6f4b6758799538e1e9138
Joshua Stresing <pyrotorch69@yahoo.com>
2022-06-14 09:08:29 -0700

    got some basic file and data setup, making db next

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,12 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP table if exists books;
+DROP table if exists authors;
+
+CREATE table books (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,
+    released INT NOT NULL
+);
+
+INSERT INTO books (name) VALUES ('Spice and Wolf', 'Isuna Hasekura', '2006')
