71cbe77092f1b596b816ec2fbee9ed55da982043
Joshua Stresing <pyrotorch69@yahoo.com>
2022-06-14 14:15:59 -0700

    books is all hooked up, showing on page, test file returning properly and passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,19 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('list of books', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('should fetch and return a list of books', async () => {
+    const res = await request(app).get('/books');
+    expect(res.body.length).toEqual(2);
+    const saw = res.body.find((char) => char.id === '1');
+    expect(saw).toHaveProperty('name', 'Spice and Wolf');
+    expect(saw).toHaveProperty('released', 2006);
   });
+
   afterAll(() => {
     pool.end();
   });
