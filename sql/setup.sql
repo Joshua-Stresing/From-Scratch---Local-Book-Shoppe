2caf6fa425b1ab34efd3f6605525edc702f8720b
Joshua Stresing <pyrotorch69@yahoo.com>
2022-06-14 10:21:39 -0700

    db is working and set up

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,12 +1,24 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP table if exists books;
-DROP table if exists authors;
+DROP TABLE if exists books;
+DROP TABLE if exists authors;
 
-CREATE table books (
+CREATE TABLE books (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR NOT NULL,
+    author VARCHAR NOT NULL,
     released INT NOT NULL
 );
 
-INSERT INTO books (name) VALUES ('Spice and Wolf', 'Isuna Hasekura', '2006')
+CREATE TABLE authors (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,
+    dob VARCHAR NOT NULL
+);
+
+INSERT INTO books (name, author, released)
+VALUES ('Spice and Wolf', 'Isuna Hasekura', 2006), ('Mushoku Tensei', 'Rifujin na Magonote', 2014);
+
+INSERT INTO authors (name, dob)
+VALUES ('Isuna Hasekura', '1982'), ('Rifujin na Magonote', 'N/A');
+
